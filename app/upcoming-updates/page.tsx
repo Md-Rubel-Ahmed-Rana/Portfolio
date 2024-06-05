c974ba04a46efd479279e01748c6210b0822848c
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2024-06-05 12:12:41 +0600

    color issue solved on updates

--- app/upcoming-updates/page.tsx
+++ app/upcoming-updates/page.tsx
@@ -6,22 +6,23 @@ const Updates = async () => {
   return (
     <div className="min-h-screen lg:p-8 p-2 bg-gray-100 flex justify-center items-center">
       <div className="w-full max-w-4xl bg-white shadow-lg rounded-lg p-6">
-        <h3 className="text-4xl font-bold text-gray-700 text-center">
+        <h3 className="lg:text-4xl text-2xl font-bold text-gray-700 text-center">
           Upcoming updates
         </h3>
         <ul className="space-y-8 mt-10">
           {data.map((update) => (
             <li key={update.id} className="shadow rounded-lg p-4">
-              <h4 className="text-lg font-medium mb-2">
-                {update.title}{" "}
+              <h4 className="text-lg text-gray-600 font-medium mb-2">
+                {update.title}
                 <span className="text-gray-400 font-sans">
-                  {" "}
-                  {`(${update.category})`}{" "}
+                  {`(${update.category})`}
                 </span>
               </h4>
-              <p className="text-gray-700 mb-2">{update.description}</p>
-              <p className="text-sm mb-2">Status: {update.status}</p>
-              <p className="text-sm text-gray-500">
+              <p className="text-gray-600 mb-2">{update.description}</p>
+              <p className="text-sm mb-2 text-gray-600">
+                Status: {update.status}
+              </p>
+              <p className="text-sm text-gray-600">
                 Priority: {update.priority}
               </p>
             </li>
