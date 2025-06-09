7ecad2ce71977115c3ac80d211433cb823c062f4
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-06-09 11:43:22 +0600

    user feedback dashboard initiated

---
+++ feedback-dashboard/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-2xl">Welcome to Feedback Dashboard</h1>
+    </div>
+  );
+}
